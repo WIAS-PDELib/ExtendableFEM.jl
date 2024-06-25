5e253aea791b0b381bd779fab022d05922bc8f39
Documenter.jl <documenter@juliadocs.github.io>
2024-06-25 21:05:45 +0000

    build based on 328e1ad

---
+++ v0.5.0/module_examples/Example245_NSEFlowAroundCylinder.jl
@@ -0,0 +1,207 @@
+#=
+
+# 245 : Flow around a cylinder
+([source code](@__SOURCE_URL__))
+
+This example solves the DFG Navier-Stokes benchmark problem
+```math
+\begin{aligned}
+- \mu \Delta \mathbf{u} + (\mathbf{u} \cdot \nabla) \mathbf{u} + \nabla p & = 0\\
+\mathrm{div}(\mathbf{u}) & = 0
+\end{aligned}
+```
+on a rectangular 2D domain with a circular obstacle, see
+[here](https://www.mathematik.tu-dortmund.de/~featflow/en/benchmarks/cfdbenchmarking/flow/dfg_benchmark1_re20.html) for details.
+
+This script demonstrates the employment of external grid generators and the computation of drag and lift coefficients.
+
+Note: This example needs the additional packages Triangulate and SimplexGridFactory to generate the mesh.
+
+![](example245.png)
+
+=#
+
+
+module Example245_NSEFlowAroundCylinder
+
+using ExtendableFEM
+using Triangulate
+using SimplexGridFactory
+using ExtendableGrids
+using GridVisualize
+using LinearAlgebra
+using Test #hide
+
+## inlet data for Karman vortex street example
+## as in DFG benchmark 2D-1 (Re = 20, laminar)
+const umax = 0.3
+const umean = 2 // 3 * umax
+const L, W, H = 0.1, 2.2, 0.41
+function inflow!(result, qpinfo)
+	x = qpinfo.x
+	result[1] = 4 * umax * x[2] * (H - x[2]) / (H * H)
+	result[2] = 0.0
+end
+
+function kernel_nonlinear!(result, u_ops, qpinfo)
+	u, ∇u, p = view(u_ops, 1:2), view(u_ops, 3:6), view(u_ops, 7)
+	μ = qpinfo.params[1]
+	result[1] = dot(u, view(∇u, 1:2))
+	result[2] = dot(u, view(∇u, 3:4))
+	result[3] = μ * ∇u[1] - p[1]
+	result[4] = μ * ∇u[2]
+	result[5] = μ * ∇u[3]
+	result[6] = μ * ∇u[4] - p[1]
+	result[7] = -(∇u[1] + ∇u[4])
+	return nothing
+end
+
+## everything is wrapped in a main function
+function main(; Plotter = nothing, μ = 1e-3, maxvol = 1e-3, reconstruct = true, kwargs...)
+
+	## load grid (see function below)
+	xgrid = make_grid(W, H; n = Int(ceil(sqrt(1 / maxvol))), maxvol = maxvol)
+
+	## problem description
+	PD = ProblemDescription()
+	u = Unknown("u"; name = "velocity")
+	p = Unknown("p"; name = "pressure")
+	id_u = reconstruct ? apply(u, Reconstruct{HDIVRT1{2}, Identity}) : id(u)
+
+	assign_unknown!(PD, u)
+	assign_unknown!(PD, p)
+	assign_operator!(PD, NonlinearOperator(kernel_nonlinear!, [id_u, grad(u), id(p)]; params = [μ], kwargs...))
+	assign_operator!(PD, InterpolateBoundaryData(u, inflow!; regions = 4))
+	assign_operator!(PD, HomogeneousBoundaryData(u; regions = [1, 3, 5]))
+
+	## P2-bubble + reconstruction operator
+	FETypes = [H1P2B{2, 2}, H1P1{1}]
+
+	## generate FESpaces and Solution vector
+	FES = [FESpace{FETypes[1]}(xgrid), FESpace{FETypes[2]}(xgrid; broken = true)]
+
+	## solve
+	sol = solve(PD, FES; maxiterations = 50, target_residual = 1e-10)
+
+	## postprocess : compute drag/lift (see function below)
+	draglift = get_draglift(sol, μ)
+	pdiff = get_pressure_difference(sol)
+	println("[drag, lift] = $draglift")
+	println("p difference = $pdiff")
+
+	## plots via GridVisualize
+	plt = GridVisualizer(; Plotter = Plotter, layout = (4, 1), clear = true, size = (800, 1200))
+	gridplot!(plt[1, 1], xgrid, linewidth = 1)
+	gridplot!(plt[2, 1], xgrid, linewidth = 1, xlimits = [0, 0.3], ylimits = [0.1, 0.3])
+	scalarplot!(plt[3, 1], xgrid, nodevalues(sol[u]; abs = true)[1, :])
+	vectorplot!(plt[3, 1], xgrid, eval_func_bary(PointEvaluator([id(u)], sol)), spacing = (0.2, 0.05), vscale = 0.5, clear = false)
+	scalarplot!(plt[4, 1], xgrid, view(nodevalues(sol[p]), 1, :), levels = 11, title = "p_h")
+
+	return [draglift[1], draglift[2], pdiff[1]], plt
+end
+
+function get_pressure_difference(sol::FEVector)
+	xgrid = sol[2].FES.xgrid
+	PE = PointEvaluator([id(2)], sol)
+	p_left = zeros(Float64, 1)
+	x1 = [0.15, 0.2]
+	p_right = zeros(Float64, 1)
+	x2 = [0.25, 0.2]
+	evaluate!(p_left, PE, x1)
+	evaluate!(p_right, PE, x2)
+	@show p_left, p_right
+	return p_left - p_right
+end
+
+function get_draglift(sol::FEVector, μ)
+
+	## this function is interpolated for drag/lift test function creation
+	function DL_testfunction(component)
+		function closure(result, qpinfo)
+			x = qpinfo.x
+			fill!(result, 0)
+			if sqrt((x[1] - 0.2)^2 + (x[2] - 0.2)^2) <= 0.06
+				result[component] = 1
+			end
+		end
+	end
+
+	## drag lift calcuation by testfunctions
+	function draglift_kernel(result, input, qpinfo)
+		## input = [ u, grad(u), p , v , grad(v)]
+		##         [1:2,  3:6,   7 ,8:9,  10:13 ]
+		result[1] = μ * (input[3] * input[10] + input[4] * input[11] + input[5] * input[12] + input[6] * input[13])
+		result[1] += (input[1] * input[3] + input[2] * input[4]) * input[8]
+		result[1] += (input[1] * input[5] + input[2] * input[6]) * input[9]
+		result[1] -= input[7] * (input[10] + input[13])
+		result[1] *= -(2 / (umean^2 * L))
+		return nothing
+	end
+	DLIntegrator = ItemIntegrator(draglift_kernel, [id(1), grad(1), id(2), id(3), grad(3)]; quadorder = 4)
+
+	## test for drag
+	TestFunction = FEVector(sol[1].FES; name = "drag/lift testfunction")
+	interpolate!(TestFunction[1], ON_BFACES, DL_testfunction(1))
+	drag = sum(evaluate(DLIntegrator, [sol[1], sol[2], TestFunction[1]]))
+
+	## test for lift
+	interpolate!(TestFunction[1], ON_BFACES, DL_testfunction(2))
+	lift = sum(evaluate(DLIntegrator, [sol[1], sol[2], TestFunction[1]]))
+
+	return [drag, lift]
+end
+
+## grid generator script using SimplexGridBuilder/Triangulate
+function make_grid(W, H; n = 20, maxvol = 0.1)
+	builder = SimplexGridBuilder(Generator = Triangulate)
+	function circlehole!(builder, center, radius; n = 20)
+		points = [point!(builder, center[1] + radius * sin(t), center[2] + radius * cos(t)) for t in range(0, 2π, length = n)]
+		for i ∈ 1:n-1
+			facet!(builder, points[i], points[i+1])
+		end
+		facet!(builder, points[end], points[1])
+		holepoint!(builder, center)
+	end
+	p1 = point!(builder, 0, 0)
+	p2 = point!(builder, W, 0)
+	p3 = point!(builder, W, H)
+	p4 = point!(builder, 0, H)
+
+	## heuristic refinement around cylinder
+	refine_radius = 0.25
+	maxrefinefactor = 1 // 20
+	function unsuitable(x1, y1, x2, y2, x3, y3, area)
+		if area > maxvol * min(max(4 * maxrefinefactor, abs((x1 + x2 + x3) / 3 - 0.2)), 1 / maxrefinefactor)
+			return true
+		end
+		dist = sqrt(((x1 + x2 + x3) / 3 - 0.2)^2 + ((y1 + y2 + y3) / 3 - 0.2)^2) - 0.05
+		myarea = dist < refine_radius ? maxvol * max(maxrefinefactor, 1 - (refine_radius - dist) / refine_radius) : maxvol
+		if area > myarea
+			return true
+		else
+			return false
+		end
+	end
+
+	facetregion!(builder, 1)
+	facet!(builder, p1, p2)
+	facetregion!(builder, 2)
+	facet!(builder, p2, p3)
+	facetregion!(builder, 3)
+	facet!(builder, p3, p4)
+	facetregion!(builder, 4)
+	facet!(builder, p4, p1)
+	facetregion!(builder, 5)
+	circlehole!(builder, (0.2, 0.2), 0.05, n = n)
+
+	simplexgrid(builder, maxvolume = 16 * maxvol, unsuitable = unsuitable)
+end
+
+generateplots = default_generateplots(Example245_NSEFlowAroundCylinder, "example245.png") #hide
+function runtests() #hide
+	dragliftpdiff, plt = main(; maxvol = 5e-3) #hide
+	@test dragliftpdiff[1] ≈ 5.484046680249255 #hide
+	@test dragliftpdiff[2] ≈ 0.006508486071976145 #hide
+	@test dragliftpdiff[3] ≈ 0.1203441600631075 #hide
+end #hide
+end
---
+++ v0.5.0/module_examples/Example260_AxisymmetricNavierStokesProblem.jl
@@ -0,0 +1,155 @@
+#=
+
+# 260 : Axisymmetric Stokes
+([source code](@__SOURCE_URL__))
+
+This example solves the 3D stagnation point flow via 
+the 2.5D axisymmetric formulation of the Navier--Stokes problem that seeks a velocity ``\mathbf{u} = (u_z, u_r)``
+and pressure ``p`` such that
+```math
+\begin{aligned}
+- \mu\left(\partial^2_r + r^{-1} \partial_r + \partial^2_z - r^{-2} \right) u_r
++ (u_r \partial_r + u_z \partial_z) u_r + \partial_r p & = \mathbf{f}_r\\
+- \mu\left(\partial^2_r + r^{-1} \partial_r + \partial^2_z \right) u_z
++ (u_r \partial_r + u_z \partial_z) u_z + \partial_z p & = \mathbf{f}_z\\
+(\partial_r + r^{-1})u_r + \partial_z u_z & = 0
+\end{aligned}
+```
+with exterior force ``\mathbf{f}`` and some viscosity parameter ``\mu``.
+
+The axisymmetric formulation assumes that the velocity in some
+3D-domain, that is obtained by rotation of a 2D domain ``\Omega``,
+only depends on the distance ``r`` to the rotation axis and the
+``z``-coordinate tangential to the x-axis, but not on the angular coordinate
+of the cylindric coordinates.
+The implementation employs ``r``-dependent bilinear forms and a
+Cartesian grid for the 2D ``(z,r)`` domain that is assumed to be rotated
+around the ``r=0``-axis.
+
+This leads to the weak formulation
+```math
+\begin{aligned}
+a(u,v) + b(p,v) & = (f,v) \\
+         b(q,u) & = 0
+\end{aligned}
+```
+with the bilinear forms
+```math
+\begin{aligned}
+a(u,v) := \int_{\Omega} \left( \nabla u : \nabla v + r^{-2} u_r v_r \right) r dr dz\\
+b(q,v) := \int_{\Omega} q \left( \mathrm{div}(v) + r^{-1} u_r \right) r dr dz
+\end{aligned}
+```
+where the usual Cartesian differential operators can be used. The factor ``2\pi`` from the
+integral over the rotation angle drops out on both sides.
+
+The computed solution for the default parameters looks like this:
+
+![](example260.png)
+
+=#
+
+module Example260_AxisymmetricNavierStokesProblem
+
+using ExtendableFEM
+using ExtendableGrids
+using SimplexGridFactory
+using Triangulate
+using Test #hide
+
+
+function kernel_convection!(result, input, qpinfo)
+    u, ∇u = view(input, 1:2), view(input, 3:6)
+    r = qpinfo.x[1]
+    result[1] = r*(∇u[1]*u[1] + ∇u[2]*u[2])
+    result[2] = r*(∇u[3]*u[1] + ∇u[4]*u[2])
+    return nothing
+end
+
+function kernel_stokes_axisymmetric!(result, u_ops, qpinfo)
+    u, ∇u, p = view(u_ops,1:2), view(u_ops,3:6), view(u_ops, 7)
+    r = qpinfo.x[1]
+    μ = qpinfo.params[1]
+    ## add Laplacian
+    result[1] = μ/r * u[1] - p[1]
+    result[2] = 0
+    result[3] = μ*r * ∇u[1] - r*p[1]
+    result[4] = μ*r * ∇u[2]
+    result[5] = μ*r * ∇u[3]
+    result[6] = μ*r * ∇u[4] - r*p[1]
+    result[7] = -(r*(∇u[1]+∇u[4]) + u[1])
+    return nothing
+end
+
+function u!(result, qpinfo)
+    x = qpinfo.x
+    result[1] = x[1]
+    result[2] = -2*x[2]
+end
+
+function kernel_l2div(result, u_ops, qpinfo)
+    u, divu = view(u_ops,1:2), view(u_ops,3)
+    result[1] = (qpinfo.x[1]*divu[1] + u[1])^2
+end
+
+
+function main(; μ = 0.1, nrefs = 4, nonlinear = false, uniform = false, Plotter = nothing, kwargs...)
+
+    ## problem description
+    PD = ProblemDescription()
+    u = Unknown("u"; name = "velocity")
+    p = Unknown("p"; name = "pressure")
+    assign_unknown!(PD, u)
+    assign_unknown!(PD, p)
+    assign_operator!(PD, BilinearOperator(kernel_stokes_axisymmetric!, [id(u),grad(u),id(p)]; params = [μ], kwargs...))#; jacobian = kernel_jacobian!)) 
+    if nonlinear
+        assign_operator!(PD, NonlinearOperator(kernel_convection!, [id(u)], [id(u),grad(u)]; bonus_quadorder = 1, kwargs...))#; jacobian = kernel_jacobian!)) 
+    end
+    assign_operator!(PD, InterpolateBoundaryData(u, u!; regions = [3]))
+    assign_operator!(PD, HomogeneousBoundaryData(u; regions = [4], mask = (1,0,1)))
+    assign_operator!(PD, HomogeneousBoundaryData(u; regions = [1], mask = (0,1,1)))
+
+    ## grid
+    if uniform
+        xgrid = uniform_refine(grid_unitsquare(Triangle2D), nrefs)
+    else
+        xgrid = simplexgrid(Triangulate;
+        points=[0 0 ; 5 0 ; 5 1 ; 0 1]',
+        bfaces=[1 2 ; 2 3 ; 3 4 ; 4 1 ]',
+        bfaceregions=[1, 2, 3, 4],
+        regionpoints=[0.5 0.5;]',
+        regionnumbers=[1],
+        regionvolumes=[4.0^(-nrefs-1)])
+    end
+
+    ## solve
+    FES = [FESpace{H1P2B{2,2}}(xgrid), FESpace{L2P1{1}}(xgrid)]
+    sol = ExtendableFEM.solve(PD, FES; kwargs...)
+
+    ## compute divergence in cylindrical coordinates by volume integrals
+    DivIntegrator = ItemIntegrator(kernel_l2div, [id(u), div(u)]; quadorder = 4, resultdim = 1)
+    div_error = sqrt(sum(evaluate(DivIntegrator, sol)))
+    @info "||div(u_h)|| = $div_error"
+
+    ## compute L2error
+    function kernel_l2error(result, u_ops, qpinfo)
+        u!(result, qpinfo)
+        result .= (result - u_ops).^2
+    end
+    ErrorIntegratorExact = ItemIntegrator(kernel_l2error, [id(1)]; entities = ON_BFACES, regions = [3], quadorder = 4, kwargs...)
+    error = evaluate(ErrorIntegratorExact, sol)
+    L2error = sqrt(sum(view(error,1,:)) + sum(view(error,2,:)))
+    @info "||u - u_h|| = $L2error"
+    
+    ## plot
+    plt = plot([id(u)], sol; Plotter = Plotter)
+
+    return [div_error, L2error], plt
+end
+
+generateplots = default_generateplots(Example260_AxisymmetricNavierStokesProblem, "example260.png") #hide
+function runtests() #hide
+	errors, plt = main(; nrefs = 1) #hide
+	@test all(errors .<= 1e-12) #hide
+end #hide
+end # module
