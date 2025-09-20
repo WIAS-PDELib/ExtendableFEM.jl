d37036f90512b883af977e135eb482c86943c55e
chmerdon <christian.merdon@wias-berlin.de>
2025-09-20 15:58:32 +0200

    proximal Galerkin for obstacle problem seems to work now

--- examples/Example227_ObstacleProblemPG.jl
+++ examples/Example227_ObstacleProblemPG.jl
@@ -9,21 +9,23 @@ This example computes the solution ``u`` of the nonlinear obstacle problem that 
 	E(u) = \frac{1}{2} \int_\Omega \lvert \nabla u \rvert^2 dx - \int_\Omega f u dx
 \end{aligned}
 ```
-with some right-hand side ``f`` within the set of admissible functions that lie above an obstacle  ``\chi``
+with some right-hand side ``f`` within the set of admissible functions that lie above an obstacle ``\chi``
 ```math
 \begin{aligned}
 	\mathcal{K} := \lbrace u \in H^1_0(\Omega) : u \geq \chi \rbrace.
 \end{aligned}
 ```
 
-The obstacle constraint is realised via a penalty term
-```math
-\begin{aligned}
-	\frac{1}{\epsilon} \| \min(0, u - \chi) \|^2_{L^2}
-\end{aligned}
-```
-that is added to the energy above and is automatically differentiated for a Newton scheme.
-The computed solution for the default parameters looks like this:
+Opposite to Example225 the solution is computed by the Proximal Galerkin method
+that reformulates the problem into a solve of a series of nonlinear mixed problems.
+
+
+!!! reference
+
+	''Proximal Galerkin: A Structure-Preserving Finite Element Method for Pointwise Bound Constraints''
+	Brendan Keith, Thomas M. Surowiec, Found Comput Math (2024)
+	[>Link<](https://doi.org/10.1007/s10208-024-09681-8)
+
 
 ![](example227.png)
 =#
@@ -32,6 +34,7 @@ module Example227_ObstacleProblemPG
 
 using ExtendableFEM
 using ExtendableGrids
+using LinearAlgebra
 using Test #hide
 
 ## define obstacle and penalty kernel
@@ -73,24 +76,22 @@ function main(; Plotter = nothing, nrefs = 6, α_0 = 1.0, order = 1, parallel = 
         xgrid = partition(xgrid, RecursiveMetisPartitioning(npart = npart))
     end
 
+    ## init proximal parameter
+    params = [α_0]
+
     ## problem description
     PD = ProblemDescription()
     u = Unknown("u"; name = "u")
     ψ = Unknown("ψ"; name = "ψ")
-
-    ## create finite element space
-    FES = [FESpace{H1Pk{1, 2, order}}(xgrid), FESpace{H1Pk{1, 2, order}}(xgrid)]
-
-    ## init proximal parameter
-    params = [α_0]
     assign_unknown!(PD, u)
     assign_unknown!(PD, ψ)
     assign_operator!(PD, BilinearOperator(kernel_blf!, [grad(u)]; params = params, parallel = parallel, kwargs...))
-    assign_operator!(PD, LinearOperator([id(u)], [(id(ψ))]; transposed_copy = 1, store = true, parallel = parallel, kwargs...))
-    #assign_operator!(PD, LinearOperator(kernel_lf!, [id(u)], [id(ψ)]; parallel = parallel, factor = 1, kwargs...))
+    assign_operator!(PD, BilinearOperator([id(u)], [(id(ψ))]; transposed_copy = 1, store = true, parallel = parallel, kwargs...))
     assign_operator!(PD, NonlinearOperator(∇R!, [id(ψ)], [id(ψ)]; parallel = parallel, factor = -1, bonus_quadorder = 5, kwargs...))
     assign_operator!(PD, HomogeneousBoundaryData(u; regions = 1:4, kwargs...))
 
+    ## create finite element space
+    FES = [FESpace{H1Pk{1, 2, order}}(xgrid), FESpace{H1Pk{1, 2, order}}(xgrid)]
 
     ## add coupling matrix
     M = FEMatrix(FES[1], FES[2])
@@ -98,21 +99,31 @@ function main(; Plotter = nothing, nrefs = 6, α_0 = 1.0, order = 1, parallel = 
     assemble!(M, BilinearOperator([id(1)], [id(1)]))
     assign_operator!(PD, LinearOperator(b, [u]; factor = 1, kwargs...))
 
-
     ## solve
     sol = FEVector(FES; tags = PD.unknowns)
+    sol_prev = FEVector(FES; tags = PD.unknowns)
     SC = nothing
-    r = 1.1 #3//2
-    q = 1.1 #3//2
-    for k in 1:1
-        ## set b vector
+    r = 3 // 2
+    q = 3 // 2
+    converged = false
+    k = 0
+    while !converged
+        ## save previous solution and update right-hand side
         b.entries .= M.entries * view(sol[ψ])
-        view(sol[ψ]) .= 0
+        sol_prev.entries .= sol.entries
 
         ## solve nonlinear problem
-        sol, SC = solve(PD, FES, SC; init = sol, maxiterations = 10, return_config = true, kwargs...)
-        params[1] = min(max(r^(q^k) - params[1]), 10^2)
-        @info "Step $k: α = $(params[1])", sol
+        k += 1
+        @info "Step $k: α = $(params[1])"
+        sol, SC = solve(PD, FES, SC; init = sol, maxiterations = 20, return_config = true, kwargs...)
+        dist = norm(view(sol[u]) .- view(sol_prev[u]))
+        @info "dist = $dist"
+        if dist < 1.0e-10
+            converged = true
+        else
+            # increase proximal parameter
+            params[1] = min(max(r^(q^k) - params[1]), 10^2)
+        end
     end
 
 
